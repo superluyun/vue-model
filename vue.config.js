b0a6324f06bcaa4840e9397150ba8ae5653bd38c
ly <1437505934@qq.com>
2020-11-26 18:07:19 +0800

    .

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    chainWebpack: config => {
+        const oneOfsMap = config.module.rule('scss').oneOfs.store
+        oneOfsMap.forEach(item => {
+            item
+                .use('sass-resources-loader')
+                .loader('sass-resources-loader')
+                .options({
+                    // Provide path to the file with resources
+                    // 要公用的scss的路径
+                    resources: './src/assets/css/common.scss'
+                })
+                .end()
+        })
+    }
+}
